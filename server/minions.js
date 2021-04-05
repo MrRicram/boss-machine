10c6716dfaf996097a9edb20d2e8c862c3b0bd9f
Ricardo Ramos <rf.ramos95@gmail.com>
2021-04-05 19:36:51 +0100

    Implement work features

--- server/minions.js
+++ server/minions.js
@@ -2,6 +2,7 @@ const minionsRouter = require('express').Router();
 
 module.exports = minionsRouter;
 
+const e = require('express');
 const {
     addToDatabase,
     getAllFromDatabase,
@@ -17,9 +18,21 @@ minionsRouter.param('minionId', (req, res, next, id) => {
         req.minion = minion;
         next();
     }
-    else    
+    else  {
         res.status(404).send();
+    }  
 });
+
+minionsRouter.param('workId', (req, res, next, id) => {
+    const work = getFromDatabaseById('work', id);
+    if (work) {
+        req.workId = work;
+        next();
+    }
+    else {
+        res.status(404).send();
+    }
+})
 
 minionsRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('minions'));
@@ -46,5 +59,39 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
     else
         res.status(500);
     
+    res.send();
+});
+
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const work = getAllFromDatabase('work').filter(job => job.minionId === req.params.minionId);
+    res.send(work);
+});
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    const workToAdd = req.body;
+    workToAdd.minionId = req.params.minionId;
+    const newWork = addToDatabase('work', workToAdd);
+    res.status(201).send(newWork);
+});
+
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if (req.params.minionId !== req.body.minionId) {
+        res.status(400).send();
+    }
+    else {
+        const updatedWork = updateInstanceInDatabase('work', req.body);
+        res.status(201).send(updatedWork);
+    }
+});
+
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('work', req.params.workId);
+    if (deleted) {
+        res.status(204);
+    }
+    else {
+        res.status(500);
+    }
+
     res.send();
 });
